0a95220ef67beabe2c74cf49aeb14da7cfd98acd
Armando Hernandez <armhzjz@pm.me>
2022-04-14 13:02:34 +0200

    User name, UID and GID

--- c/.devcontainer/build_dockerimage.sh
+++ c/.devcontainer/build_dockerimage.sh
@@ -7,4 +7,8 @@ else
     USERNAME=$1
 fi
 
-DOCKER_BUILDKIT=1 docker build --network host --build-arg USER=$USERNAME --secret id=token,src=$HOME/.config/exercism/user.json --tag devel-c:bullseye .
+DOCKER_BUILDKIT=1 docker build --network host \
+    --build-arg USER=$USERNAME \
+    --build-arg UID=$(id -u) \
+    --build-arg GID=$(id -g) \
+    --secret id=token,src=$HOME/.config/exercism/user.json --tag devel-c:bullseye .
