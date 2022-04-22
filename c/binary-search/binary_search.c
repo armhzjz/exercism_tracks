6d851c2dc61ab00eaef9f7b9e89b437b7c88591a
Armando Hernandez <armhzjz@pm.me>
2022-04-22 23:07:09 +0200

    binary search - no curly brackets on if's

--- c/binary-search/binary_search.c
+++ c/binary-search/binary_search.c
@@ -5,14 +5,13 @@ int *binary_search(int value, const int *arr, size_t length) {
     if(length == 0)
         return NULL;
 
-    if(value == arr[length >> 1]) {
+    if(value == arr[length >> 1])
         return (int *) &arr[length >> 1];
-    } else if(value < arr[length >> 1]){
+    else if(value < arr[length >> 1])
         return binary_search(value, arr, (length >> 1));
-    } else if(value > arr[length >> 1]) {
+    else if(value > arr[length >> 1])
         return length>>1 ? binary_search(value, &arr[(length >> 1)], length - (length >> 1)) : 
-                            binary_search(value, &arr[(length >> 1)], 0);
-    }
+                            NULL;
 
     return NULL; /* intended to avoid warnings/errors,
                     but in theory should never be needed nor used.
