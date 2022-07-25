12b01880134121dffc9056781b7ae816696f4430
Armando Hernandez <armhzjz@pm.me>
2022-07-25 22:22:38 +0200

    devcontainer.json - settings.json

---
+++ c/all-your-base/all_your_base.c
@@ -0,0 +1,15 @@
+#include "all_your_base.h"
+
+
+unsigned long rebase(
+        signed char* digits,
+        signed short input_base,
+        signed short output_base,
+        unsigned long input_length) {
+    if(!digits)
+        return -1;
+    
+    if(input_base != 10) {
+        
+    }
+}
---
+++ c/all-your-base/all_your_base.h
@@ -0,0 +1,10 @@
+#ifndef ALL_YOUR_BASE_H
+#define ALL_YOUR_BASE_H
+
+
+#define DIGITS_ARRAY_SIZE 64
+
+
+unsigned long rebase(signed char*, signed short, signed short, unsigned long);
+
+#endif
