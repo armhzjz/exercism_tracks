54dac626c3e03b762a3598d9cd0053417006e135
Armando Hernandez <armhzjz@pm.me>
2022-09-10 19:02:18 +0000

    all-your-base

--- c/all-your-base/all_your_base.c
+++ c/all-your-base/all_your_base.c
@@ -1,4 +1,9 @@
+#include <math.h>
 #include "all_your_base.h"
+
+
+unsigned long convert_input_to_base_10(signed char*, unsigned long, signed short);
+unsigned long DecToBase(signed short, signed long, signed char*);
 
 
 unsigned long rebase(
@@ -8,8 +13,46 @@ unsigned long rebase(
         unsigned long input_length) {
     if(!digits)
         return -1;
-    
-    if(input_base != 10) {
-        
+    if(input_length == 0 || input_base <= 1 || output_base <= 1)
+        return 0;
+
+    return DecToBase(output_base, convert_input_to_base_10(digits, input_length, input_base), digits);
+}
+
+unsigned long convert_input_to_base_10(signed char* digits, unsigned long input_length, signed short input_base) {
+    signed long accumulative = 0;
+    for(signed long digit_position = input_length -1; 0 <= digit_position; digit_position--) {
+        /* if the digit is a negative number or its value happens to be greater than the input base */
+        if(digits[input_length - digit_position - 1] < 0 || digits[input_length - digit_position - 1] >= input_base)
+            return -1;
+        accumulative += (pow(input_base, digit_position) * digits[input_length - digit_position - 1]);
     }
+    return accumulative;
+}
+
+unsigned long DecToBase(signed short output_base, signed long decnum, signed char* digits) {
+    // unsigned long num = 0, remainder = 0, exp = 0;
+    unsigned long remainder = 0;
+    unsigned int digit_counter = 0;
+    unsigned int quotient = decnum;
+    signed char reverse_digits[DIGITS_ARRAY_SIZE];
+
+    if(decnum == 0)
+        return 1;
+
+    /*  if a negative digit was found during the converstion to base 10 
+        or a digit was found to be greater than its input base */
+    if(decnum < 0)
+        return 0;
+
+    while(quotient != 0) {
+        remainder = quotient % output_base;
+        reverse_digits[digit_counter++] = remainder;
+        quotient = (unsigned int) (quotient / output_base);
+    }
+
+    for(signed int idx = digit_counter - 1; idx + 1 != 0; idx --)
+        digits[idx] = reverse_digits[digit_counter - idx - 1];
+    
+    return digit_counter;
 }
--- c/all-your-base/test_all_your_base.c
+++ c/all-your-base/test_all_your_base.c
@@ -27,6 +27,9 @@ static void check_rebase(int16_t input_base, int8_t input_digits[],
                          size_t input_length, int16_t output_base,
                          int8_t expected_digits[], size_t expected_length)
 {
+   // int8_t input[] = { 0 };
+   // int8_t expected[] = { 0 };
+   // check_rebase(1, input, LENGTH(input), 10, expected, 0);
    int8_t digits[DIGITS_ARRAY_SIZE] = { 0 };
 
    copy_array(input_digits, digits, input_length);
@@ -48,7 +51,7 @@ static void test_single_bit_to_decimal(void)
 
 static void test_binary_to_single_decimal(void)
 {
-   TEST_IGNORE();   // delete this line to run test
+   // TEST_IGNORE();   // delete this line to run test
    int8_t input[] = { 1, 0, 1 };
    int8_t expected[] = { 5 };
    check_rebase(2, input, LENGTH(input), 10, expected, LENGTH(expected));
@@ -56,7 +59,7 @@ static void test_binary_to_single_decimal(void)
 
 static void test_single_decimal_to_binary(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    int8_t input[] = { 5 };
    int8_t expected[] = { 1, 0, 1 };
    check_rebase(10, input, LENGTH(input), 2, expected, LENGTH(expected));
@@ -64,7 +67,7 @@ static void test_single_decimal_to_binary(void)
 
 static void test_binary_to_multiple_decimal(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    int8_t input[] = { 1, 0, 1, 0, 1, 0 };
    int8_t expected[] = { 4, 2 };
    check_rebase(2, input, LENGTH(input), 10, expected, LENGTH(expected));
@@ -72,7 +75,7 @@ static void test_binary_to_multiple_decimal(void)
 
 static void test_decimal_to_binary(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    int8_t input[] = { 4, 2 };
    int8_t expected[] = { 1, 0, 1, 0, 1, 0 };
    check_rebase(10, input, LENGTH(input), 2, expected, LENGTH(expected));
@@ -80,7 +83,7 @@ static void test_decimal_to_binary(void)
 
 static void test_trinary_to_hexadecimal(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    int8_t input[] = { 1, 1, 2, 0 };
    int8_t expected[] = { 2, 10 };
    check_rebase(3, input, LENGTH(input), 16, expected, LENGTH(expected));
@@ -88,7 +91,7 @@ static void test_trinary_to_hexadecimal(void)
 
 static void test_hexadecimal_to_trinary(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    int8_t input[] = { 2, 10 };
    int8_t expected[] = { 1, 1, 2, 0 };
    check_rebase(16, input, LENGTH(input), 3, expected, LENGTH(expected));
@@ -96,7 +99,7 @@ static void test_hexadecimal_to_trinary(void)
 
 static void test_15_bit_integer(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    int8_t input[] = { 3, 46, 60 };
    int8_t expected[] = { 6, 10, 45 };
    check_rebase(97, input, LENGTH(input), 73, expected, LENGTH(expected));
@@ -104,14 +107,14 @@ static void test_15_bit_integer(void)
 
 static void test_empty_list(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    int8_t input[DIGITS_ARRAY_SIZE] = { 0 };
    TEST_ASSERT_EQUAL_size_t(0, rebase(input, 2, 10, 0));
 }
 
 static void test_single_zero(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    int8_t input[] = { 0 };
    int8_t expected[] = { 0 };
    check_rebase(2, input, LENGTH(input), 10, expected, LENGTH(expected));
@@ -119,7 +122,7 @@ static void test_single_zero(void)
 
 static void test_multiple_zeros(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    int8_t input[] = { 0, 0, 0 };
    int8_t expected[] = { 0 };
    check_rebase(10, input, LENGTH(input), 2, expected, LENGTH(expected));
@@ -127,7 +130,7 @@ static void test_multiple_zeros(void)
 
 static void test_leading_zeros(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    int8_t input[] = { 0, 6, 0 };
    int8_t expected[] = { 4, 2 };
    check_rebase(7, input, LENGTH(input), 10, expected, LENGTH(expected));
@@ -135,7 +138,7 @@ static void test_leading_zeros(void)
 
 static void test_input_base_is_one(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    int8_t input[] = { 0 };
    int8_t expected[] = { 0 };
    check_rebase(1, input, LENGTH(input), 10, expected, 0);
@@ -143,7 +146,7 @@ static void test_input_base_is_one(void)
 
 static void test_input_base_is_zero(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    int8_t input[] = { 0 };
    int8_t expected[] = { 0 };
    check_rebase(0, input, LENGTH(input), 10, expected, 0);
@@ -151,7 +154,7 @@ static void test_input_base_is_zero(void)
 
 static void test_input_base_is_negative(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    int8_t input[] = { 1 };
    int8_t expected[] = { 0 };
    check_rebase(-2, input, LENGTH(input), 10, expected, 0);
@@ -159,7 +162,7 @@ static void test_input_base_is_negative(void)
 
 static void test_negative_digit(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    int8_t input[] = { 1, -1, 1, 0, 1, 0 };
    int8_t expected[] = { 0 };
    check_rebase(2, input, LENGTH(input), 10, expected, 0);
@@ -167,7 +170,7 @@ static void test_negative_digit(void)
 
 static void test_invalid_positive_digit(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    int8_t input[] = { 1, 2, 1, 0, 1, 0 };
    int8_t expected[] = { 0 };
    check_rebase(2, input, LENGTH(input), 10, expected, 0);
@@ -175,7 +178,7 @@ static void test_invalid_positive_digit(void)
 
 static void test_output_base_is_one(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    int8_t input[] = { 1, 0, 1, 0, 1, 0 };
    int8_t expected[] = { 0 };
    check_rebase(2, input, LENGTH(input), 1, expected, 0);
@@ -183,7 +186,7 @@ static void test_output_base_is_one(void)
 
 static void test_output_base_is_zero(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    int8_t input[] = { 7 };
    int8_t expected[] = { 0 };
    check_rebase(10, input, LENGTH(input), 0, expected, 0);
@@ -191,7 +194,7 @@ static void test_output_base_is_zero(void)
 
 static void test_output_base_is_negative(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    int8_t input[] = { 1 };
    int8_t expected[] = { 0 };
    check_rebase(2, input, LENGTH(input), -7, expected, 0);
@@ -199,7 +202,7 @@ static void test_output_base_is_negative(void)
 
 static void test_both_bases_are_negative(void)
 {
-   TEST_IGNORE();
+   // TEST_IGNORE();
    int8_t input[] = { 1 };
    int8_t expected[] = { 0 };
    check_rebase(-2, input, LENGTH(input), -7, expected, 0);
